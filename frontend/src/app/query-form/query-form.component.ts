6a3cda67bb6fb6acee9abafa15d0431d36ed6056
Daniel Miller <danielmiller@Daniels-MacBook-Pro.local>
2023-11-29 00:47:53 -0500

    Setup angular frontend with service and component templates

---
+++ frontend/src/app/query-form/query-form.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-query-form',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './query-form.component.html',
+  styleUrl: './query-form.component.css'
+})
+export class QueryFormComponent {
+
+}
---
+++ frontend/src/app/response-display/response-display.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-response-display',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './response-display.component.html',
+  styleUrl: './response-display.component.css'
+})
+export class ResponseDisplayComponent {
+
+}
