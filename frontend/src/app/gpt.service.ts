e53232a8a80915e252cd0c6048526618f113e8ce
Daniel Miller <danielmiller@Daniels-MBP.lan>
2023-11-29 00:58:03 -0500

    Create angular service to communicate with backend

--- frontend/src/app/gpt.service.ts
+++ frontend/src/app/gpt.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class GptService {
+  private apiUrl = 'http://localhost:5050/api/ask';
+  
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  askGPT(query: string) {
+    return this.http.post<any>(this.apiUrl, { query });
+  }
 }
